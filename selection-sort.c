a7ffe5650fc76f241eac176db849108e8d0840fd
Emily Burley <emilylesburley@gmail.com>
2024-04-11 22:14:48 -0400

    Change from OMP to regular time tracking

--- selection-sort.c
+++ selection-sort.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
-#include "omp.h"
+#include <time.h>
 
 const int N = 100000;
 
@@ -13,11 +13,6 @@ typedef struct entry {
 } entry;
 
 entry INIT_ENTRY = { -1, INT_MAX };
-
-// The custom reduction behavior for `entry` values.
-#pragma omp declare reduction(min_entry : entry : \
-  omp_out = omp_in.value < omp_out.value ? omp_in : omp_out) \
-  initializer(omp_priv={-1,INT_MAX})
 
 void swap(int *a, int *b) {
   int temp = *a;
@@ -32,7 +27,6 @@ void selection_sort(int *data) {
     iter_min.index = i;
     iter_min.value = data[i];
 
-    #pragma omp parallel for reduction(min_entry:iter_min)
     for (int j = i + 1; j < N; j++) {
       if (data[j] < iter_min.value) {
         iter_min.value = data[j];
@@ -45,6 +39,9 @@ void selection_sort(int *data) {
 }
 
 int main() {
+
+  struct timespec start, stop;
+
   // initialize the input array to all random values
   int* data = (int*)malloc(N * sizeof(int));
   for (int i = 0; i < N; i++) {
@@ -52,9 +49,9 @@ int main() {
     //data[i] = rand() % 100;
   }
 
-  double start = omp_get_wtime();
+  clock_gettime(CLOCK_REALTIME, &start);
   selection_sort(data);
-  double duration = omp_get_wtime() - start;
+  clock_gettime(CLOCK_REALTIME, &stop);
 
   // check that the array is sorted in increasing order
   // by comparing each pair of consecutive elements
@@ -62,6 +59,8 @@ int main() {
   for (int i = 1; i < N; i++) {
     valid = valid && (data[i] >= data[i - 1]);
   }
+
+  double duration = (stop.tv_sec - start.tv_sec) + (stop.tv_nsec - start.tv_nsec) / 1e9;
 
   printf("Result valid: %s\n", valid ? "yes" : "no");
   printf("Values sorted: %d\n", N);
